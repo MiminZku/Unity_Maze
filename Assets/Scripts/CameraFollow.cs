73c60cdf3aa160843edfa6a939d1eda7c225e478
MiminZku <82875083+MiminZku@users.noreply.github.com>
2023-01-05 08:19:43 +0900

    map 1

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = target.transform.position + new Vector3(-2f, 1.5f, 0f);
+        gameObject.transform.LookAt(target.transform.position + new Vector3(0,1f,0));
+    }
+}
---
+++ Assets/Scripts/PlayerContoller.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContoller : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    float h, v;
+    public bool isMove;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+        if (h * v != 0)
+        {
+            isMove = true;
+        }
+        else
+        {
+            isMove = false;
+        }
+        gameObject.transform.Translate(new Vector3(h, 0, v)*moveSpeed*Time.deltaTime);
+        gameObject.transform.LookAt(new Vector3(h, 0, v));
+    }
+    
+    
+}
