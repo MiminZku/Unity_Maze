56b1a491c6e2e8da36d6d25453c2e31d4e0f26c9
MiminZku <82875083+MiminZku@users.noreply.github.com>
2023-01-05 14:15:59 +0900

    player. camera move done

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,20 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
     public GameObject target;
+    public float rotateSpeed;
+    float mouseX;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.position = target.transform.position + new Vector3(0f, 1.5f, -2f);
-        gameObject.transform.LookAt(target.transform.position + new Vector3(0,1f,0));
+        
+        gameObject.transform.LookAt(target.transform.position);
+
+        if (Input.GetMouseButton(1))
+        {
+            Cursor.visible = false;
+            mouseX = Input.GetAxis("Mouse X");
+            gameObject.transform.RotateAround(target.transform.position, new Vector3(0, mouseX, 0), rotateSpeed *10* Time.deltaTime);
+        }
+        Cursor.visible = true;
+
     }
 }
--- Assets/Scripts/PlayerContoller.cs
+++ Assets/Scripts/PlayerContoller.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class PlayerContoller : MonoBehaviour
 {
-    public float moveSpeed = 10f;
+    public float moveSpeed;
+    public float rotateSpeed;
+    public Animator animator;
+    public GameObject camera;
+    //public Rigidbody rb;
+
     float h, v;
     public bool isMove;
+    Vector3 moveDirection;
+    Vector3 movement;
+    Quaternion lookRotation;
     // Start is called before the first frame update
     void Start()
     {
-        
+        lookRotation= transform.rotation;
     }
 
     // Update is called once per frame
@@ -18,17 +26,24 @@ public class PlayerContoller : MonoBehaviour
     {
         h = Input.GetAxisRaw("Horizontal");
         v = Input.GetAxisRaw("Vertical");
-        if (h * v != 0)
+        moveDirection = transform.position - new Vector3(camera.transform.position.x, transform.position.y, camera.transform.position.z);
+        movement = moveDirection * v + Vector3.Cross(Vector3.up, moveDirection) * h;
+        movement = movement.normalized * moveSpeed * Time.deltaTime;
+
+        //rb.MovePosition(transform.position + movement);
+        //rb.rotation = Quaternion.Slerp(rb.rotation, lookRotation, rotateSpeed * Time.deltaTime);
+        transform.position += movement;
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotateSpeed * Time.deltaTime);
+
+        if (h != 0 || v != 0)
         {
-            isMove = true;
+            animator.SetBool("isMove",true);
+            lookRotation = Quaternion.LookRotation(movement);
         }
         else
         {
-            isMove = false;
+            animator.SetBool("isMove", false);
         }
-        Debug.Log(string.Format("{0} , {1}",h,v));
-        gameObject.transform.Translate(new Vector3(h, 0, v)*moveSpeed*Time.deltaTime);
-
     }
     
     
