cbe1f071df9699db67c65e2c75ee1e68dd50929c
MiminZku <82875083+MiminZku@users.noreply.github.com>
2023-01-10 00:24:38 +0900

    random maze console

---
+++ Assets/Scripts/MazeGenerator.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
+using UnityEngine;
+
+public class MazeGenerator : MonoBehaviour
+{
+    public GameObject floorPrefab;
+    public GameObject wallPrefab;
+
+    public GameObject[] floors;
+    public GameObject[] walls;
+
+    public static int rowNum = 10;
+    public static int colNum = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rowNum = 2 * rowNum - 1;
+        colNum= 2 * colNum - 1;
+
+        int[][] maze;
+        while(true)
+        {
+            maze = GenerateMaze();
+            if (FindPath(maze, 0, 0))
+            {
+                break;
+            }
+        }
+        /*for(int i = 0; i< rowNum; i++)
+        {
+            string temp = " ";
+            for (int j = 0; j < colNum; j++)
+            {
+                temp = temp + " " + maze[i][j];
+            }
+            Debug.Log(temp);
+        }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    static bool FindPath(int[][] maze, int row, int col)
+    {
+        if (row == rowNum-1 && col == colNum-1)
+        {
+            maze[row][col] = 2;
+            return true;
+        }
+        maze[row][col] = 2;
+        if (col + 1 < maze[0].Length && 0 == maze[row][col + 1])
+            if (FindPath(maze, row, col + 1))
+                return true;
+        if (row + 1 < maze.Length && 0 == maze[row + 1][col])
+            if (FindPath(maze, row + 1, col))
+                return true;
+        if (col - 1 >= 0 && 0 == maze[row][col - 1])
+            if (FindPath(maze, row, col - 1))
+                return true;
+        if (row - 1 >= 0 && 0 == maze[row - 1][col])
+            if (FindPath(maze, row - 1, col))
+                return true;
+        maze[row][col] = 0;
+        return false;
+    }
+
+    static int[][] GenerateMaze()
+    {
+        int[][] maze = new int[rowNum][];
+        for (int i = 0; i < colNum; i++)
+        {
+            maze[i] = new int[colNum];
+        }
+        for(int row = 0; row < rowNum; row++)
+        {
+            int[] temp = new int[colNum];
+            for(int col = 0; col < colNum; col++)
+            {
+                if(row%2 == 0)
+                {
+                    if(col % 2 == 0)
+                    {
+                        temp[col] = 0;
+                    }
+                    else
+                    {
+                        temp[col] = Random.Range(0, 2);
+                    }
+                }
+                else
+                {
+                    if (col % 2 == 0)
+                    {
+                        temp[col] = Random.Range(0, 2);
+                    }
+                    else
+                    {
+                        temp[col] = 1;
+                    }
+                }
+            }
+            maze[row] = temp;
+        }
+        return maze;
+    }
+
+    void Build(int[][] maze)
+    {
+
+    }
+}
